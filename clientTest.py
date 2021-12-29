27ef5de63a1524f21fd44955a6ef7da191e3bd4e
Dor7777 <perezdo@post.bgu.ac.il>
2021-12-29 21:21:33 +0200

    changes

--- clientTest.py
+++ clientTest.py
@@ -1,14 +1,12 @@
 from socket import *
 import struct
-import keyboard
-import sys
-import select
+
 
 from serverTest import FORMAT
 
 HEADER = 0xabcddcba
 MESSAGETYPE = 0x2
-UDP_PORT = 13117
+UDP_PORT = 2088
 BUFF_SIZE = 2<<10
 team_name = "Desperate Programming Apes"
 
@@ -24,27 +22,36 @@ class Client:
         clientSocketUDP.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #brodcasting op
         print("Client started, listening for offer requests...")
         clientSocketUDP.bind(("", UDP_PORT)) #bind ip/port
+
         self.serverConUDPHandler(clientSocketUDP) #send to connection handler
 
     def serverConUDPHandler (self,clientSocketUDP):
         try:
             pack, address = clientSocketUDP.recvfrom(BUFF_SIZE)
-            print("Received offer from " + address[0]+", attempting to connect..")
+            print("Received offer from " + address[0] +  " attempting to connect..")
             message = struct.unpack("IBH", pack)
+       #     print("here0")
             self.connect_to_TCP(('127.0.0.1', message[2]))
+        #    print("here3")
         except:
+         #   print("error")
             pass
 
 
     def connect_to_TCP(self,address):
+       # print("here1")
         clientSocketTCP = socket(AF_INET, SOCK_STREAM)
+       # print("here2")
         clientSocketTCP.connect((address[0],address[1]))
+       # print("connected")
         clientSocketTCP.send(str.encode(team_name+'\n'))
+       # print("send my name")
         while True:
-            message = clientSocketTCP.recv(BUFF_SIZE)
-            print(message.decode(FORMAT))
-            input = keyboard.read_key()
-            clientSocketTCP.send(str.ecnode(input+'\n'))
+            message = clientSocketTCP.recv(BUFF_SIZE).decode(FORMAT)
+            print(message)
+            inputs = input()
+            clientSocketTCP.send(str.ecnode(inputs +'\n'))
+            print ("im here")
         print("Server disconnect, listen for offer requests...")
         self.connection_to_UPD()
 
